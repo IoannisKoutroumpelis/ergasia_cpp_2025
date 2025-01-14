ffd1c0087f2c189709cad7d4f45215f01c901d22
Alexandros Katsiouras <sdi2300072@di.uoa.gr>
2025-01-14 20:01:19 +0200

    elpida na kanei pass 1o unit test

--- oop24.cpp
+++ oop24.cpp
@@ -8,17 +8,18 @@ int main(int argc, char* argv[]) {
 
     Eshop shop(argv[1], argv[2], argv[3]);
     string choice;
-
+    cout << "\nWelcome to the e-shop!!!\n";
     do {
-        cout << "\nWelcome to the e-shop!!!\n";
         cout << "Do you want to login or register? (enter option):";
         cin >> choice;
 
         if(choice == "login") {
             shop.login();
+            return 0;
         }
         else if(choice == "register") {
             shop.registerUser();
+            return 0;
         }
         else {
             cout << "Invalid choice. Try again.\n";
